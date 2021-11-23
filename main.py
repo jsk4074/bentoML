24446838990d9b4e6968a257aa0c4f06a2c8c972
jsk4074 <jsk4074@naver.com>
2021-11-24 01:08:21 +0900

    model saveing error

--- main.py
+++ main.py
@@ -1,23 +1,68 @@
-from sklearn import svm
-from sklearn import datasets
+import tensorflow as tf
+import tensorflow_datasets as tfds
 
-# Load training data
-iris = datasets.load_iris()
-X, y = iris.data, iris.target
+# Load training data / preprossesing
+(ds_train, ds_test), ds_info = tfds.load(
+    'mnist',
+    split=['train', 'test'],
+    shuffle_files=True,
+    as_supervised=True,
+    with_info=True,
+)
+
+def normalize_img(image, label):
+  """Normalizes images: `uint8` -> `float32`."""
+  return tf.cast(image, tf.float32) / 255., label
+
+# training pipeline
+ds_train = ds_train.map(
+    normalize_img, num_parallel_calls=tf.data.AUTOTUNE)
+ds_train = ds_train.cache()
+ds_train = ds_train.shuffle(ds_info.splits['train'].num_examples)
+ds_train = ds_train.batch(128)
+ds_train = ds_train.prefetch(tf.data.AUTOTUNE)
+
+# evaluation pipeline
+ds_test = ds_test.map(
+    normalize_img, num_parallel_calls=tf.data.AUTOTUNE)
+ds_test = ds_test.batch(128)
+ds_test = ds_test.cache()
+ds_test = ds_test.prefetch(tf.data.AUTOTUNE)
+
+# # Load training data
+# iris = datasets.load_iris()
+# X, y = iris.data, iris.target
 
 # Model Training
-clf = svm.SVC(gamma='scale')
-clf.fit(X, y)
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape=(28, 28)),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dense(10)
+])
+model.compile(
+    optimizer=tf.keras.optimizers.Adam(0.001),
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=[tf.keras.metrics.SparseCategoricalAccuracy()],
+)
 
+model.fit(
+    ds_train,
+    epochs=6,
+    validation_data=ds_test,
+)
 
-  # import the IrisClassifier class defined above
-  from iris_classifier import IrisClassifier
-	
-  # Create a iris classifier service instance
-  iris_classifier_service = IrisClassifier()
-	
-  # Pack the newly trained model artifact
-  iris_classifier_service.pack('model', clf)
-	
-  # Save the prediction service to disk for model serving
-  saved_path = iris_classifier_service.save()
+# Model Training
+# clf = svm.SVC(gamma='scale')
+# clf.fit(X, y)
+
+# import the MnistClassifier class defined above
+from mnist_classifier import MnistClassifier
+
+# Create a iris classifier service instance
+Mnist_classifier_service = MnistClassifier()
+
+# Pack the newly trained model artifact
+Mnist_classifier_service.pack('model', model)
+
+# Save the prediction service to disk for model serving
+saved_path = Mnist_classifier_service.save()
